1b7f45a719f88a962b4efca8afe864b33cf5cc95
0xdef1cafe <88504456+0xdef1cafe@users.noreply.github.com>
2024-10-23 13:46:45 +1100

    now at java errors

---
+++ fix-android-build-tools.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+ln -s "$ANDROID_HOME/build-tools/31.0.0/d8" "$ANDROID_HOME/build-tools/31.0.0/dx"
+ln -s "$ANDROID_HOME/build-tools/31.0.0/lib/d8.jar" "$ANDROID_HOME/build-tools/31.0.0/lib/dx.jar"
