cbb47ef15cfbabf7b9d91acc022fe09e8ad0e728
Warden Clyffe <admin@wardenclyffe.net>
2024-11-25 16:51:46 -0500

    dev: add fix-yoga post install script. and add it to package.json

---
+++ scripts/fix-yoga.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+const path = require('path')
+
+const yogaFilePath = path.resolve(
+  __dirname,
+  '../node_modules/react-native/ReactCommon/yoga/yoga/Yoga.cpp'
+)
+
+const targetLine = 'node->getLayout().hadOverflow() |'
+const replacementLine = 'node->getLayout().hadOverflow() ||'
+
+fs.readFile(yogaFilePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading Yoga.cpp:', err)
+    process.exit(1)
+  }
+
+  if (data.includes(targetLine)) {
+    const updatedData = data.replace(targetLine, replacementLine)
+
+    fs.writeFile(yogaFilePath, updatedData, 'utf8', (writeErr) => {
+      if (writeErr) {
+        console.error('Error writing Yoga.cpp:', writeErr)
+        process.exit(1)
+      }
+      console.log('Yoga.cpp successfully patched.')
+    })
+  } else {
+    console.log('Yoga.cpp already patched or target line not found.')
+  }
+})
